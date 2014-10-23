bd4535f027945f8b5db1a62a7376f5d12c4693d3
Ann Fang En <anfangen@pc208.inflab.tuwien.ac.at>
2014-10-23 17:06:23 +0200

    assignment 1

---
+++ ex1.m
@@ -0,0 +1,46 @@
+function ex1()
+    dir = 'res/';
+    names = {'00125v', '00149v', '00153v', ...
+        '00351v', '00398v', '01112v'};
+
+    rn = '_R.jpg';
+    gn = '_G.jpg';
+    bn = '_B.jpg';
+
+    for k = 1:length(names)
+        R = readimg(dir, names{k}, rn);
+        G = readimg(dir, names{k}, gn);
+        B = readimg(dir, names{k}, bn);
+
+        s1 = ncc(R, G);
+        s2 = ncc(R, B);
+
+        G = circshift(G, s1);
+        B = circshift(B, s2);
+
+        I = R;
+        I(:, :, 2) = G;
+        I(:, :, 3) = B;
+
+        imwrite(I, strcat(names{k}, '.png'));
+    end
+end
+
+function [I] = readimg(dir, name, ext)
+    I = imread(strcat(dir, strcat(name, ext)));
+end
+
+function [s, maxcorr] = ncc(I1, I2)
+    maxcorr = 0;
+    s = [0 0];
+    for i = -15:15
+        for j = -15:15
+            I2tmp = circshift(I2, [i j]);
+            corr = corr2(I1(15:(end-15), 15:(end-15)), I2tmp(15:(end-15), 15:(end-15))); % this is great
+            if corr > maxcorr
+                maxcorr = corr;
+                s = [i j];
+            end
+        end
+    end
+end
