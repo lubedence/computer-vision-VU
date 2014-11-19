adf3ad9794a1689afa553ac39c40cc121b598ddf
Thomas Lang <thomaslang0@gmail.com>
2014-11-19 17:22:10 +0100

    ex2 geht; vis noch nicht fertig

--- ex3.m
+++ ex3.m
@@ -6,6 +6,7 @@ levels = 10;
 in_dir = 'res/';
 out_dir = 'ex3_out/';
 I = im2double(imread(strcat(in_dir, filename)));
+Result = zeros(size(I,1), size(I,2), 2);
 scale_space = zeros(size(I,1), size(I,2), levels);
 
 function [f] = level_filter(lvl)
@@ -16,11 +17,55 @@ end
 
 for lvl = 1:levels
     %figure; surf(level_filter(lvl));
-    scale_space(:,:,lvl) = imfilter(I, level_filter(lvl), 'same', 'replicate');
+    scale_space(:,:,lvl) = abs(imfilter(I, level_filter(lvl), 'same', 'replicate'));
 end
 
-for lvl = 2:(levels-1)
-    % step 2
+%window_max = @(S,i,j) max(max(S(i-1:i+1, j-1:j+1)));
+threshold = 0.3
+
+% calculate window maxima
+scale_space_max = zeros(size(scale_space));
+for lvl = 1:levels
+    %scale_space_max = ordfilt2(scale_space(:,:,lvl),9,ones(3,3));
+    scale_space_max(:,:,lvl) = imdilate(scale_space(:,:,lvl), ones(3,3));
 end
+
+disp('maxima');
+
+n = 1;
+for lvl = 2:(levels-1)
+    for i = 2:(size(I,1)-1)
+        for j = 2:(size(I,2)-1)
+            current_px = scale_space(i,j,lvl);
+            if current_px < threshold
+                continue;
+            end
+            
+            max_prev = scale_space_max(i,j,lvl-1);
+            max_next = scale_space_max(i,j,lvl+1);
+            max_current = scale_space_max(i,j,lvl);
+            
+            if current_px == max_current && current_px > max_prev && current_px > max_next
+                if current_px > Result(i,j,1)
+                    Result(i,j,1) = current_px;
+                    Result(i,j,2) = sigma_0 * k^lvl * sqrt(2);
+                    %results(n,1:4) = [i,j,sigma_0 * k^lvl * sqrt(2),current_px];
+                    %n = n + 1;
+                end
+                
+            end
+        end
+    end
+end
+
+% Vis = zeros(size(I,1), size(I,2), 3);
+% Vis(:,:,1) = I;
+% Vis(:,:,2) = Result;
+% figure; imshow(Vis);
+% figure; imshow(Result);
+[cy, cx] = find(Result(:,:,2));
+%radii = Result(sub2ind(size(Result(:,:,2)), cx, cy), 2);
+%radii = Result(find(Result(:,:,2)), 2);
+show_all_circles(I, cx, cy, radii);
 
 end
