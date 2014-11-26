2c23732e255752dd8cb2a2715d48d7795c288c8e
Thomas Lang <thomaslang0@gmail.com>
2014-11-26 16:53:16 +0100

    impl ex3 issue 2 plot

--- ex3.m
+++ ex3.m
@@ -3,28 +3,68 @@ function ex3(filename, threshold, scaletest)
 % threshold: for example 0.2
 % scaletest: set 1 to additionally use half-sized image
 
+
+sigma_0 = 2;
+k = 1.25;
+levels = 10;
 in_dir = 'res/';
 out_dir = 'ex3_out/';
+
+if scaletest
+    % make image size even
+    I = imread(strcat(in_dir, filename));
+    if mod(size(I,1), 2) == 1
+        I = I(1:(end-1), :);
+    end
+    if mod(size(I,2), 2) == 1
+        I = I(:, 1:(end-1));
+    end
+    filename = sprintf('e_%s.png', filename);
+    imwrite(I, strcat(in_dir, filename));
+end
 
 [scale_space, Result] = blob_det(filename);
 if scaletest
     % resize image and save with 'half_' prefix
-    name_half = sprintf('half_%s', filename);
-    I_tmp = imread(strcat(in_dir, filename));
-    imwrite(imresize(I_tmp, 0.5), strcat(in_dir, name_half));
+    name_half = sprintf('half_%s.png', filename);
+    
+    I_resized = imresize(imread(strcat(in_dir, filename)), 0.5);
+    imwrite(I_resized, strcat(in_dir, name_half));
     [scale_space_half, Result_half] = blob_det(name_half);
     
-    % plot responses for both images for a common key point
-    % first_common_index = find(Result(:,:,1) & Result_half(:,:,1));
-    % TODO how can we find the same keypoint in both images?
+    % find first common key point
+    Result_half = imresize(Result_half, 2, 'nearest');
+    common_indices = find(Result(:,:,1) & Result_half(:,:,1));
+    first_common_index = common_indices(1);
+    
+    % get LoG responses of common key point for every level
+    p = zeros(levels);
+    p_half = zeros(levels);
+    sig_ = zeros(levels);
+    for lvl = 1:levels
+        S = scale_space(:,:,lvl);
+        S_half = imresize(scale_space_half(:,:,lvl), 2, 'nearest');
+        p(lvl) = S(first_common_index);
+        p_half(lvl) = S_half(first_common_index);
+        sig_(lvl) = sigma_0 * k^lvl;
+    end
+    
+    % plot them
+    figure; 
+    subplot(2,1,1);
+    plot(sig_, p);
+    title('Responses of keypoint in original image');
+    xlabel('\sigma');
+    ylabel('LoG response');
+    subplot(2,1,2);
+    plot(sig_, p_half);
+    title('Responses of keypoint in half sized image');
+    xlabel('\sigma');
+    ylabel('LoG response');
 end
 
 function [scale_space, Result] = blob_det(filename)
     tic;
-
-    sigma_0 = 2;
-    k = 1.25;
-    levels = 10;
     
     I = im2double(imread(strcat(in_dir, filename)));
     Result = zeros(size(I,1), size(I,2), 2);
@@ -77,7 +117,7 @@ function [scale_space, Result] = blob_det(filename)
             end
         end
     end
-
+    
     % visualize
     [cy, cx] = find(Result(:,:,2)); % get position vectors
     Result2 = Result(:,:,2);
