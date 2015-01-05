ca2da9356c08dc1570948911123a0bdb4e9848ef
Thomas Lang <wstcode@gmail.com>
2015-01-05 02:24:02 +0100

    BuildKNN, main

--- UE2/ass5_data/BuildVocabulary.m
+++ UE2/ass5_data/BuildVocabulary.m
@@ -5,35 +5,25 @@ NUM_IMAGES = 800;
 
 descrs_overall = zeros(128, NUM_FEATURES * NUM_IMAGES);
 current_index = 1;
+filepaths = ImagePaths(folder);
 
-dir_info = dir(folder);
-for i = 1:length(dir_info)
-    if dir_info(i).name(1) == '.'
-        continue;
-    end
-    
-    subfolder = sprintf('%s/%s', folder, dir_info(i).name);
-    fprintf('entering %s...\n', subfolder);
-    subdir_info = dir(subfolder);
-    for j = 1:length(subdir_info)
-        if subdir_info(j).name(1) == '.'
-            continue;
-        end
-        
-        filepath = sprintf('%s/%s', subfolder, subdir_info(j).name);
-        I = im2single(imread(filepath));
-        stepsize = sqrt(size(I,1) * size(I,2) / (NUM_FEATURES * 1.2));
-        [frames, descrs] = vl_dsift(I, 'Step', stepsize, 'Fast');
-        % take random subsample of size NUM_FEATURES
-        y = randsample(size(descrs,2), NUM_FEATURES);
-        descrs_overall(:, current_index:(current_index + NUM_FEATURES - 1)) = descrs(:, y);
-        current_index = current_index + NUM_FEATURES;
-    end
+wb_handle = waitbar(0, 'Building Vocabulary...');
+
+for i = 1:length(filepaths)
+    I = im2single(imread(filepaths{i}.Path));
+    stepsize = sqrt(size(I,1) * size(I,2) / (NUM_FEATURES * 1.2));
+    [~, descrs] = vl_dsift(I, 'Step', stepsize, 'Fast');
+    % take random subsample of size NUM_FEATURES
+    y = randsample(size(descrs,2), NUM_FEATURES);
+    descrs_overall(:, current_index:(current_index + NUM_FEATURES - 1)) = descrs(:, y);
+    current_index = current_index + NUM_FEATURES;
+    waitbar(i / NUM_IMAGES);
 end
 
 fprintf('k-means...\n');
-[C, A] = vl_kmeans(single(descrs_overall), num_clusters);
+[C, ~] = vl_kmeans(single(descrs_overall), num_clusters);
 
 fprintf('vocabulary complete!\n\n');
+close(wb_handle);
 
 end
